3b2c4bbf30dd68ef22d080c92f0933b9c43bf183
Afif Zafri <zafri_21596@yahoo.com>
2018-10-26 23:16:14 +0800

    create user profile page

--- template/header.php
+++ template/header.php
@@ -47,7 +47,7 @@
                 <li><a href="contactus.php">Contact</a></li>
                 <li class="menu-has-children"><a href="">Profile</a>
                   <ul>
-                    <li><a href="elements.html">View Profile</a></li>
+                    <li><a href="./profile.php">View Profile</a></li>
                     <li><a href="./dashboard">Dashboard</a></li>
                     <li><a href="search.html">Setting</a></li>
                     <li><a href="single.html">Log Out</a></li>
