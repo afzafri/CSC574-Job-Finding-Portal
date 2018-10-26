ad7287ef8fb015002717d94b8cd021fc6d900f2b
Afif Zafri <zafri_21596@yahoo.com>
2018-10-26 19:06:39 +0800

    created header and footer template

---
+++ template/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+  <!-- Mobile Specific Meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Favicon-->
+  <link rel="shortcut icon" href="./template/img/fav.png">
+  <!-- Author Meta -->
+  <meta name="author" content="codepixer">
+  <!-- Meta Description -->
+  <meta name="description" content="">
+  <!-- Meta Keyword -->
+  <meta name="keywords" content="">
+  <!-- meta character set -->
+  <meta charset="UTF-8">
+  <!-- Site Title -->
+  <title><?php echo $pageTitle; ?> | Job Finding Portal</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+    CSS
+    ============================================= -->
+    <link rel="stylesheet" href="./template/css/linearicons.css">
+    <link rel="stylesheet" href="./template/css/font-awesome.min.css">
+    <link rel="stylesheet" href="./template/css/bootstrap.css">
+    <link rel="stylesheet" href="./template/css/magnific-popup.css">
+    <link rel="stylesheet" href="./template/css/nice-select.css">
+    <link rel="stylesheet" href="./template/css/animate.min.css">
+    <link rel="stylesheet" href="./template/css/owl.carousel.css">
+    <link rel="stylesheet" href="./template/css/main.css">
+  </head>
+  <body>
+
+      <header id="header" id="home">
+        <div class="container">
+          <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+              <a href="index.html"><h3 style="color: white">Job Finding Portal</h3></a>
+            </div>
+            <nav id="nav-menu-container">
+              <ul class="nav-menu">
+                <li class="menu-active"><a href="index.php">Home</a></li>
+                <li><a href="category.html">Job Search</a></li>
+                <li><a href="category.html">Companies List</a></li>
+                <li><a href="about-us.html">About Us</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li class="menu-has-children"><a href="">Profile</a>
+                  <ul>
+                    <li><a href="elements.html">View Profile</a></li>
+                    <li><a href="search.html">Setting</a></li>
+                    <li><a href="single.html">Log Out</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav><!-- #nav-menu-container -->
+          </div>
+        </div>
+      </header><!-- #header -->
+
+    <!-- start banner Area -->
+    <section class="banner-area relative" id="home">
+      <div class="overlay overlay-bg"></div>
+      <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+          <div class="about-content col-lg-12">
+            <h1 class="text-white">
+              <?php echo $pageTitle; ?>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End banner Area -->
