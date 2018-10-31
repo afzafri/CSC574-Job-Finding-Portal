450ae3b0957aa5bdc4c46c67cb0d67f6208e10ed
Afif Zafri <zafri_21596@yahoo.com>
2018-10-31 11:10:54 +0800

    rule check if login, show form, and dashboard link

--- index.php
+++ index.php
@@ -12,20 +12,25 @@
       <!-- list post -->
       <div class="col-lg-8 post-list blog-post-list">
 
-        <form class="form-area single-widget" id="myForm" action="mail.php" method="post" class="contact-form text-right">
-          <div class="row">
-            <div class="col-lg-12 form-group">
-              <input name="title" placeholder="Enter post Title" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter post Title'" class="common-input mb-20 form-control" required="" type="text">
+        <?php
+          if($login == 1) {
+            ?>
+            <form class="form-area single-widget" id="myForm" action="mail.php" method="post" class="contact-form text-right">
+              <div class="row">
+                <div class="col-lg-12 form-group">
+                  <input name="title" placeholder="Enter post Title" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter post Title'" class="common-input mb-20 form-control" required="" type="text">
 
-              <textarea class="common-textarea mt-10 form-control" name="experience" placeholder="Share your job experience" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Share your job experience'" required=""></textarea>
+                  <textarea class="common-textarea mt-10 form-control" name="experience" placeholder="Share your job experience" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Share your job experience'" required=""></textarea>
 
-              <input name="image" class="common-input mb-20 form-control" type="file">
+                  <input name="image" class="common-input mb-20 form-control" type="file">
 
-              <button class="primary-btn mt-20 text-white" style="float: right;">Post Job Done</button>
-              <div class="mt-20 alert-msg" style="text-align: left;"></div>
-            </div>
-          </div>
-        </form>
+                  <button class="primary-btn mt-20 text-white" style="float: right;">Post Job Done</button>
+                  <div class="mt-20 alert-msg" style="text-align: left;"></div>
+                </div>
+              </div>
+            </form>
+            <?php
+          } ?>
 
         <?php
 
