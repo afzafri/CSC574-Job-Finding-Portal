27785239cacc60facae832c6803b51aca0b331b7
Afif Zafri <zafri_21596@yahoo.com>
2018-11-19 22:34:10 +0800

    done register user account

--- login.php
+++ login.php
@@ -1,3 +1,86 @@
+<?php
+
+ob_start();
+//Start session
+session_start();
+include './config.php';
+
+$errormsg = "";
+
+if(isset($_POST['signup'])) {
+
+  $email = $_POST['email'];
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $password_confirm = $_POST['password_confirm'];
+  $level = $_POST['level'];
+
+  if($email != "" && $username != "" && $password != "" && $password_confirm != "") {
+
+    try
+    {
+      // check if email already registered
+      $stmt = $conn->prepare("SELECT * FROM LOGIN WHERE L_EMAIL = ?");
+      $stmt->execute(array($email));
+
+      if(!$stmt->fetch(PDO::FETCH_ASSOC)) {
+        // email not registered, register new
+
+        // check if password matched
+        if($password != $password_confirm)
+        {
+          echo "<script>alert('Password not match. Please check')</script>";
+          $errormsg = "<center><h4><font color='red'>Password not match. Please check</font></h4></center>";
+        } else {
+          // insert into login table
+          $stmt = $conn->prepare("INSERT INTO
+                                  LOGIN (L_EMAIL, L_USERNAME, L_PASSWORD, L_LEVEL)
+                                  VALUES (?, ?, ?, ?) ");
+
+          $stmt->execute(array($email, $username, md5($password), $level));
+
+          // get the login id
+          $loginid = $conn->lastInsertId();
+
+          // insert into youngster or job provider table
+          if($level == 2) {
+            $stmt = $conn->prepare("INSERT INTO
+                                    JOB_PROVIDER (L_ID)
+                                    VALUES (?) ");
+
+            $stmt->execute(array($loginid));
+          } else if($level == 3) {
+            $stmt = $conn->prepare("INSERT INTO
+                                    JOB_SEEKER (L_ID)
+                                    VALUES (?) ");
+
+            $stmt->execute(array($loginid));
+          }
+
+            echo "
+            <script>
+            alert('New account successfully registered.');
+            </script>";
+        }
+
+      } else {
+        // email already registered
+        echo "<script>alert('Email address already in used.')</script>";
+        $errormsg = "<center><h4><font color='red'>Email address already in used.</font></h4></center>";
+      }
+
+    }
+    catch(PDOException $e)
+    {
+      echo "Connection failed : " . $e->getMessage();
+    }
+
+  } else {
+    $errormsg = "<center><h4><font color='red'>Please fill in all the information.</font></h4></center>";
+  }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en" >
 
@@ -10,7 +93,6 @@
   <link rel='stylesheet' href='./template/css/font-awesome.min.css'>
 
       <link rel="stylesheet" href="./template/css/loginregister.css">
-
 
 </head>
 
@@ -36,27 +118,28 @@
         <a href="#" class="forgot-password">Forgot password?</a>
       </div>
     </form>
-    <form class="email-signup" method="post">
+    <form class="email-signup" method="post" action="./login.php">
       <div class="u-form-group">
-        <input type="email" placeholder="Email" name="email"/>
+        <input type="email" placeholder="Email" name="email" required/>
       </div>
       <div class="u-form-group">
-        <input type="text" placeholder="Username" name="username"/>
+        <input type="text" placeholder="Username" name="username" required/>
       </div>
       <div class="u-form-group">
-        <input type="password" placeholder="Password" name="password"/>
+        <input type="password" placeholder="Password" name="password" required/>
       </div>
       <div class="u-form-group">
-        <input type="password" placeholder="Confirm Password" name="password_confirm"/>
+        <input type="password" placeholder="Confirm Password" name="password_confirm" required/>
       </div>
       <div class="u-form-group">
-        <select name="level" require>
+        <select name="level" required>
           <option value="2">Job Provider</option>
           <option value="3">Job Seeker</option>
         </select>
       </div>
       <div class="u-form-group">
-        <button>Sign Up</button>
+        <button type="submit" name="signup">Sign Up</button>
+        <?php echo $errormsg; ?>
       </div>
     </form>
   </div>
